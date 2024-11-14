68ccb150af2db396a3722f1ae12b83c2d96a23ce
Jha <shrijha@deloitte.com>
2024-11-14 23:07:51 +0530

    Basic argo setup with UI

--- kcc/scripts/enable_kcc_gke.sh
+++ kcc/scripts/enable_kcc_gke.sh
@@ -1,4 +1,9 @@
 #For existing cluster
 expost CLUSTER_NAME='demo-kcc-cluster'
+export REGION='us-central1'
 gcloud container clusters update CLUSTER_NAME \
-    --update-addons ConfigConnector=ENABLED
+    --update-addons ConfigConnector=ENABLED \
+    --region REGION
+
+#kcc is not supported for autopilot clusters
+
